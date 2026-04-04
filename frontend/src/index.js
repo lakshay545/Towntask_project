b6426cf05e6adb24ae0be19427afb01ab1e615cc
lakshay545 <lakshaysh1+work@gmail.com>
2026-04-04 21:16:21 +0530

    Temporary Clear frontend directory due to error

--- frontend/src/index.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config()
-const PORT=process.env.PORT || 8000
-const app=express()
-
-app.listen(PORT,()=>{
-    console.log(`App is running on port ${PORT}`)
-})
